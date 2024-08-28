9443fd6c49346ae1d43fd0f6154615c6c578edcf
Anthony Epshtein <e4nthony@gmail.com>
2024-08-29 01:23:09 +0300

    minor improvements

--- src/background.js
+++ src/background.js
@@ -1,42 +1,28 @@
-
-function onRejected(error) {
-  console.log(`An error: ${error}`);
-}
-
+// requests full bookmarks tree
 let gettingTree = browser.bookmarks.getTree();
 
-console.log(`gettingTree:`, gettingTree);
-
-console.log(`gettingTree.array:`, gettingTree.array);
-
-gettingTree.then((promisedata) => {
-  console.log("promisedata", promisedata)         //promise itself
-  console.log("promisedata[0]", promisedata[0])   //the object "root________" of bookmarks list as promise obj
-  console.log("promisedata[0].children", promisedata[0].children)       //children array
-  console.log("promisedata[0].children[0]", promisedata[0].children[0]) //the content of "menu________"  (child of "root________")
-  console.log("promisedata[0].children[1]", promisedata[0].children[1]) //the content of "toolbar_____"  (child of "root________")
-  console.log("promisedata[0].children[1].children[1]", promisedata[0].children[1].children[1])   //the first bookmark in toolbar folder
-}, onRejected);
-
-
+/**
+ * Code bellow will generate html in Netscape-Bookmarks format. 
+ * Generator made by reverse engineering.
+ */
 html_string = gettingTree.then((bookmarksTree) => {
-
-  /* Code bellow will generate html in Netscape-Bookmarks format. Generator made by reverse engineering. */
   
-  /* This is initialization of html file that will cointain bookmarks in netscape format,
-  so browsers could read it and import using existing build in tools. */
+
+  /* This is initialization of html file that will cointain bookmarks in netscape format, so browsers could read it and import using existing build in tools. */
   let html = `
   <!DOCTYPE NETSCAPE-Bookmark-file-1>\n<!-- This is an automatically generated file by Encrypted Bookmarks Exporter Add-on. Please do not edit. -->\n<META http-equiv="Content-Type" content="text/html; charset=UTF-8">\n<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'none'; img-src data: *; object-src 'none'">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks Export</H1>\n<DL><p>\n`;
-
+  
+  /* Chrome requires indents ! */
   function makeIndent(indentLength) {
     return "\t".repeat(indentLength);
   }
 
   /**
    * function runs over all nodes in hierarchy of bookmarks.
-   * this actually is promise that can be itself folder or url, or contain children that can be folder or url
+   * this actually is promise that can be itself url or folder that contain children that can be url or folder etc..
    * 
    * @param {*} bookmarksTree - promise object with hierarchy of nested children
+   * @param {*} indent - indicates level of tree
    */
   function processTree(bookmarksTree, indent) {
     
@@ -45,7 +31,7 @@ html_string = gettingTree.then((bookmarksTree) => {
       
       if (promise.type == "folder") {
         
-        html += `${makeIndent(indent)}<DT><H3>${promise.title || "Untitled Folder"}</H3>\n${makeIndent(indent)}<DL><p>\n`;
+        html += `${makeIndent(indent)}<DT><H3>${promise.title || (indent == 1 ? "Imported Bookmarks" : "Untitled Folder" ) }</H3>\n${makeIndent(indent)}<DL><p>\n`;
         
         if (promise.children) {
           processTree(promise.children, indent + 1 );
@@ -61,13 +47,13 @@ html_string = gettingTree.then((bookmarksTree) => {
       
     }
   }
-
+  
   processTree(bookmarksTree, 1); // tree is promise that can be itself folder or url, or contain children that can be folder or url
   
-  html += `</DL><p>`;  // close html
-
-  console.log(`generated HTML:`, html);
-
+  html += `</DL><p>`;  // end of html file
+  
+  console.log('generated HTML:'); //DEBUG
+  console.log(html);              //DEBUG
   return html;
 });
 
