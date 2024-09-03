a5883a2afe82bcd006be91aee23a742613ab4101
Anthony Epshtein <e4nthony@gmail.com>
2024-09-04 00:57:17 +0300

    - redesign user interface - now tabs change color according to active mode (encryption/decryption) - added logo inside addon window

--- src/popup/tabs/tabs_script.js
+++ src/popup/tabs/tabs_script.js
@@ -4,10 +4,24 @@ document.getElementById('tab_0_imp').addEventListener('click', () => {
     let tabContents = document.getElementsByClassName("tab-content");
     tabContents[0].classList.add("active");
     tabContents[1].classList.remove("active");
+
+    var tabs = document.getElementsByClassName('tab-link');
+    // tabs[0].style.background = '#6be0db8f'; // lightblue
+    tabs[0].style.background = '#e6762c8f'; // lightorange
+    tabs[1].style.background = '#fbeee0'; // idle
+
+    document.body.style.boxShadow = 'inset 0 0 15px #e6762cb3';
 });
 
 document.getElementById('tab_1_exp').addEventListener('click', () => {
     let tabContents = document.getElementsByClassName("tab-content");
     tabContents[0].classList.remove("active");
     tabContents[1].classList.add("active");
+
+    var tabs = document.getElementsByClassName('tab-link');
+    tabs[0].style.background = '#fbeee0'; // idle
+    tabs[1].style.background = '#f1c4718f'; // yellow
+
+    document.body.style.boxShadow = 'inset 0 0 15px #f1c471f0'; //default
 });
+
